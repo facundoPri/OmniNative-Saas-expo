90a73b078086dfd257d1967de17a9aa48772f01d
facundo prieto <facundo.prieto321@gmail.com>
2020-05-22 00:16:24 -0300

    signin page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 
-import {View} from 'react-native';
+import {
+  View,
+  Platform,
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 import styles from './styles';
 
 const SignIn = () => {
-  return <View style={styles.container} />;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const passwordInput = useRef();
+
+  function handleSubmit() {
+    // SAGA(email, password)
+  }
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
+      style={styles.container}>
+      <View>
+        <Text style={styles.title}>Entrar</Text>
+        <Text style={styles.label}>E-MAIL</Text>
+        <TextInput
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          autoFocus
+          returnKeyType="next"
+          onSubmitEditing={() => passwordInput.current.focus()}
+        />
+
+        <Text style={styles.label}>SENHA</Text>
+        <TextInput
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          returnKeyType="send"
+          ref={passwordInput}
+          onSubmitEditing={handleSubmit}
+        />
+        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 };
 
 export default SignIn;
